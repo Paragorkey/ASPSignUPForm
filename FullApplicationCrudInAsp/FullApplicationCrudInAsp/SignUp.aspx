96990cccfce7d33f17b97320a2cdc85b8d8fed74
paragorkey <paragorkey1@gmail.com>
2021-12-08 23:03:57 +0530

    Insert Record success.

---
+++ FullApplicationCrudInAsp/FullApplicationCrudInAsp/SignUp.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SignUp.aspx.cs" Inherits="FullApplicationCrudInAsp.SignUp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <div class="container">
+            <div class="row">
+                <center>
+                    <h3 style="font-family: 'Times New Roman', Times, serif">Registration Form</h3>
+                </center>
+                <div class="col-md-3"></div>
+                <div class="col-md-3" style="margin-top: 15px">
+
+                    <asp:Label ID="lblid" runat="server" Text="Id"></asp:Label>
+                    <asp:TextBox ID="txtid" runat="server" CssClass="form-control" placeholder="Student Id"></asp:TextBox><br />
+                    <asp:Label ID="lblname" runat="server" Text="Name"></asp:Label>
+                    <asp:TextBox ID="txtname" runat="server" CssClass="form-control" placeholder="Enter Your Name"></asp:TextBox><br />
+                    <asp:Label ID="lblclass" runat="server" Text="Class"></asp:Label>
+                    <asp:TextBox ID="txtclass" runat="server" CssClass="form-control" placeholder="Enter Your Class"></asp:TextBox><br />
+                    <asp:Label ID="lblage" runat="server" Text="Age"></asp:Label>
+                    <asp:TextBox ID="txtage" runat="server" CssClass="form-control" placeholder="Enter Your age"></asp:TextBox><br />
+                    <asp:Label ID="lblgender" runat="server" Text="Gender"></asp:Label>
+                    <asp:RadioButtonList ID="RadioButtonList1" runat="server" DataTextField="gender" DataValueField="gender">
+                        <asp:ListItem>Male</asp:ListItem>
+                        <asp:ListItem>Female</asp:ListItem>
+                    </asp:RadioButtonList><br />
+                    <asp:Label ID="Label4" runat="server" Text="Select Image:"></asp:Label>
+                    <asp:FileUpload ID="FileUpload1" runat="server" /><br />
+                    <br />
+                    <br />
+                    <asp:Label ID="Label5" runat="server" Text="Skills:"></asp:Label><br />
+                    <asp:CheckBox ID="CheckBox1" runat="server" Text="Cricket" />
+                    <asp:CheckBox ID="CheckBox2" runat="server" Text="Football" />
+                    <asp:CheckBox ID="CheckBox3" runat="server" Text="Music" />
+                </div>
+                <div class="col-md-3" style="margin-top: 15px">
+                    <asp:Label ID="Label1" runat="server" Text="Country"></asp:Label><br />
+                    <asp:DropDownList ID="ddlCountry" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlCountry_SelectedIndexChanged" CssClass="form-control"></asp:DropDownList><br />
+                    <%--AutoPostBack="true" OnSelectedIndexChanged="ddlCountry_SelectedIndexChanged">--%><%--</asp:DropDownList><br />--%>
+                    <asp:Label ID="Label2" runat="server" Text="State"></asp:Label><br />
+                    <asp:DropDownList ID="ddlState" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlState_SelectedIndexChanged" CssClass="form-control"></asp:DropDownList><br />
+                    <%--AutoPostBack="true" OnSelectedIndexChanged="ddlState_SelectedIndexChanged">--%><%--</asp:DropDownList><br />--%>
+                    <asp:Label ID="Label3" runat="server" Text="City"></asp:Label><br />
+                    <asp:DropDownList ID="ddlCity" runat="server" AutoPostBack="true" CssClass="form-control"></asp:DropDownList><br />
+
+
+                    <asp:Label ID="lblusername" runat="server" Text="Username"></asp:Label><br />
+                    <asp:TextBox ID="txtuser" runat="server" CssClass="form-control" placeholder="Enter  Username"></asp:TextBox><br />
+
+                    <asp:Label ID="lblPass" runat="server" Text="Password"></asp:Label><br />
+                    <asp:TextBox ID="txtpass" runat="server" CssClass="form-control" placeholder="Enter Your Password" TextMode="Password"></asp:TextBox><br />
+                    <asp:Label ID="lblschool" runat="server" Text="SchoolName"></asp:Label><br />
+                    <asp:TextBox ID="txtSchool" runat="server" CssClass="form-control" placeholder="Enter Your School Name" TextMode="MultiLine"></asp:TextBox><br />
+                    <asp:Label ID="lbladdress" runat="server" Text="Address"></asp:Label><br />
+                    <asp:TextBox ID="txtAddress" runat="server" CssClass="form-control" placeholder="Enter Your Address" TextMode="MultiLine"></asp:TextBox><br />
+                </div>
+
+                <center>
+                    <asp:Button ID="btnSave" runat="server" Text="Register Student" CssClass="btn btn-primary" OnCommand="btnSave_Command" /><br />
+                </center>
+            </div>
+            <div class="col-md-3"></div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ FullApplicationCrudInAsp/FullApplicationCrudInAsp/SignUp.aspx.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+
+namespace FullApplicationCrudInAsp
+{
+    public partial class SignUp : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["CN"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                txtid.Text = GetRandomKey(6);
+                bindCountry();
+                ddlState.Items.Insert(0, "------Please Select State------");
+                ddlCity.Items.Insert(0, "------Please Select City------");
+            }
+        }
+
+        private string GetRandomKey(int len)
+        {
+            int maxSize = len;
+            char[] chars = new char[30];
+            string a;
+            a = "1234567890";
+            chars = a.ToCharArray();
+            int size = maxSize;
+            byte[] data = new byte[1];
+            RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+            crypto.GetNonZeroBytes(data);
+            size = maxSize;
+            data = new byte[size];
+            crypto.GetNonZeroBytes(data);
+            StringBuilder result = new StringBuilder(size);
+            foreach (byte b in data) { result.Append(chars[b % (chars.Length)]); }
+            return result.ToString();
+        }
+        public void bindCountry()
+        {
+
+            SqlConnection con = new SqlConnection(cs);
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from Country", con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                ddlCountry.DataSource = dt;
+                ddlCountry.DataTextField = "County";
+                ddlCountry.DataValueField = "CountryId";
+                ddlCountry.DataBind();
+                ddlCountry.Items.Insert(0, "------Please Select Country------");
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+        public void bindState()
+        {
+
+            SqlConnection con = new SqlConnection(cs);
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select State,StateId from countryState where CountryId='" + ddlCountry.SelectedValue + "'", con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                ddlState.DataSource = dt;
+                ddlState.DataTextField = "State";
+                ddlState.DataValueField = "StateId";
+                ddlState.DataBind();
+                ddlState.Items.Insert(0, "------Please Select State------");
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+        public void bindCity()
+        {
+
+            SqlConnection con = new SqlConnection(cs);
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select City,CityId from stateCity where StateId='" + ddlState.SelectedValue + "'", con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                ddlCity.DataSource = dt;
+                ddlCity.DataTextField = "City";
+                ddlCity.DataValueField = "CityId";
+                ddlCity.DataBind();
+                ddlCity.Items.Insert(0, "------Please Select City------");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        protected void ddlCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            bindState();
+        }
+        protected void ddlState_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            bindCity();
+        }
+        public void userInsert()
+        {
+            List<string> skills = new List<string>();
+            if (CheckBox1.Checked) { skills.Add(CheckBox1.Text); }
+            if (CheckBox2.Checked) { skills.Add(CheckBox2.Text); }
+            if (CheckBox3.Checked) { skills.Add(CheckBox3.Text); }
+
+            string skillsadd = string.Join(", ", skills);
+
+            SqlConnection con = new SqlConnection(cs);
+            try
+            {
+                byte[] bytes;
+                using (BinaryReader br = new BinaryReader(FileUpload1.PostedFile.InputStream))
+                {
+                    bytes = br.ReadBytes(FileUpload1.PostedFile.ContentLength);
+                }
+                con.Open();
+                SqlCommand cmd = new SqlCommand("AddStudentRecord", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", txtid.Text);
+                cmd.Parameters.AddWithValue("@Name", txtname.Text);
+                cmd.Parameters.AddWithValue("@Class", txtclass.Text);
+                cmd.Parameters.AddWithValue("@Age", txtage.Text);
+                cmd.Parameters.AddWithValue("@Gender", RadioButtonList1.SelectedItem.Text);
+                cmd.Parameters.AddWithValue("@skills", skillsadd);
+                cmd.Parameters.AddWithValue("@Image", bytes);
+                cmd.Parameters.AddWithValue("@Country", ddlCountry.SelectedItem.Text);
+                cmd.Parameters.AddWithValue("@State", ddlState.SelectedItem.Text);
+                cmd.Parameters.AddWithValue("@City", ddlCity.SelectedItem.Text);
+                cmd.Parameters.AddWithValue("@Username", txtuser.Text);
+                cmd.Parameters.AddWithValue("@Password", txtpass.Text);
+                cmd.Parameters.AddWithValue("@SchoolName", txtSchool.Text);
+                cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
+                int result = cmd.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    Response.Write("<script>alert('Record Saved Successfully')</script>");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Record Not saved!!')</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        protected void btnSave_Command(object sender, System.Web.UI.WebControls.CommandEventArgs e)
+        {
+            userInsert();
+        }
+    }
+}
